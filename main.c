82d9827390fc5dee97ba90e68f068afc5cfe6241
FedericoBustos06 <federicoo.bustosss@gmail.com>
2026-05-12 22:19:02 -0300

    Argumentos de ejecucion (1) y Resoluciones (2)
    ● Deberá poder ejecutarse utilizando argumentos a main con el fin de iniciar el mismo con diferentes factores de escala de ventana y con una resolución lógica específica (CGA o VGA).
    ● Deberá poder utilizar las siguientes resoluciones lógicas:
    ○ CGA: 320x200
    ○ VGA: 640x480

--- main.c
+++ main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "GBT/gbt.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    printf("Hello world!\n");
+    int anchoVentana = 320;
+    int altoVentana = 200;
+    int escala = 1;
+
+    gbt_iniciar();
+
+    if (argc == 3) {
+
+        int resolucion = atoi(argv[1]);
+        escala = atoi(argv[2]);
+
+        if (resolucion == 2) {
+            anchoVentana = 640;
+            altoVentana = 480;
+        }
+    } else {
+        printf("Aviso: Iniciando con valores por defecto.\n");
+        printf("Para configurar use: %s <1(CGA) o 2(VGA)> <escala>\n", argv[0]);
+    }
+    gbt_crear_ventana("Mi Tetris", anchoVentana, altoVentana, escala);
+    uint8_t corriendo = 1;
+    while (corriendo) {}
+    gbt_destruir_ventana();
+    gbt_cerrar();
     return 0;
 }
